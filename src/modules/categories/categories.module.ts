9658254dfcb861b59850a54ee1591c69f4a61176
gstvcampos <gustavo.bmc76@gmail.com>
2024-08-27 10:27:21 -0300

    fix: module categories

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
 import { HotelsModule } from '../hotels/hotels.module';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
 
 @Module({
   controllers: [CategoriesController],
-  providers: [CategoriesService],
+  providers: [CategoriesService, PrismaService],
   imports: [HotelsModule],
 })
 export class CategoriesModule {}
