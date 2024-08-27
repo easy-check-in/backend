f12e4d2d30fb5809d91bfc6f86ed3ba44dc9235e
gstvcampos <gustavo.bmc76@gmail.com>
2024-08-27 13:23:58 -0300

    feat: dto room

--- src/modules/accounts/dto/create-account.dto.ts
+++ src/modules/accounts/dto/create-account.dto.ts
@@ -1,15 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { hashSync } from 'bcryptjs';
 import { Transform } from 'class-transformer';
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateAccountDto {
   @ApiProperty({
     description: 'User name',
     example: 'johndoe',
   })
+  @IsString()
   @IsNotEmpty()
-  @IsEmail()
   username: string;
 
   @ApiProperty({
